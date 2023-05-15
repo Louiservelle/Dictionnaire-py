54e003809e86a7dd4dd90a0a221d4f4ff8389881
Louiservelle <louis.servelle@gmail.com>
2023-05-15 19:37:11 +0200

    update recherche

--- modules/rechercher.py
+++ modules/rechercher.py
@@ -2,11 +2,11 @@
 def recherche(dictionnaire):
     mot = input("Entrez un mot : ")
     
-    with open(dictionnaire, 'r') as file:
-        for line in file:
-            firstletter, word, definition = line.strip().split("#")
-            if word == mot:
-                print("Définition du mot '{}' : {}".format(word, definition))
-                return
+    file = open(dictionnaire, 'r')
+    for line in file:
+        firstletter, word, definition = line.strip().split("#")
+        if word == mot:
+            print("Définition du mot '{}' : {}\n".format(word, definition))
+            return
     
-    print("Le mot '{}' n'a pas été trouvé dans le dictionnaire.".format(mot))
+    print("Le mot '{}' n'a pas été trouvé dans le dictionnaire.\n".format(mot))
